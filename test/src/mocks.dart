2440fb6ccbd4bbd02f2e9625a7a74e2a93d5ee04
fujidaiti <fujidaiti@gmail.com>
2025-01-05 23:41:12 +0900

    wip

--- test/src/mocks.dart
+++ test/src/mocks.dart
@@ -1,7 +1,5 @@
 import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 import 'package:resizable_navigator/src/navigator_event_observer.dart';
 
-// Annotation which generates the cat.mocks.dart library and the MockCat class.
 @GenerateNiceMocks([MockSpec<NavigatorEventListener>()])
 export 'mocks.mocks.dart';
